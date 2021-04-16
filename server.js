0061c85a328b28fce829e7fb60cb8a3902604027
Mahabub Sunny <office.prosany@gmail.com>
2021-04-17 00:25:37 +0600

    Help

--- server.js
+++ server.js
@@ -19,6 +19,44 @@ app.get('/', (req, res) => {
 
 
 
+// MongoDB Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hgubw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    console.log(`${!!err ? 'Database Connection Failed' : 'Database Connection Successful'}`);
+    const usersCollection = client.db("Plumbing").collection("users");
+    const servicesCollection = client.db("Plumbing").collection("services");
+    const ordersCollection = client.db("Plumbing").collection("orders");
+    const reviewsCollection = client.db("Plumbing").collection("reviews");
+
+
+    // Check user using email
+    app.get('/verify-email-address', (req, res) => {
+        const verifyUserEmail = req.query.email;
+        console.log(verifyUserEmail)
+        usersCollection.find({ email: verifyUserEmail })
+            .toArray((err, data) => {
+                console.log('Eita ami',data)
+                if(data === []){
+                    res.send("email: nai@gmail.com")
+                }else {
+                    res.send(data)
+                }
+            })
+    });
+
+    // Create User Automatically
+    app.post('/addNewUser', (req, res) => {
+        console.log(req.body)
+        const newUserDetails = req.body;
+        usersCollection.insertOne(newUserDetails)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+});
+
+
 
 
 app.listen(port, () => {
